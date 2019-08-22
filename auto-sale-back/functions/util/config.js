252d480885fd859c7c7d3bc5c67eb0a8a4cb1741
elenems <mickhaelyarish@gmail.com>
2019-08-22 15:01:13 +0300

    Update config.js

--- auto-sale-back/functions/util/config.js
+++ auto-sale-back/functions/util/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: "AIzaSyCX4_e9Owwz1M-JAFnoU8U3gbVVhZzbJg0",
-    authDomain: "auto-sale-2e890.firebaseapp.com",
-    databaseURL: "https://auto-sale-2e890.firebaseio.com",
-    projectId: "auto-sale-2e890",
-    storageBucket: "auto-sale-2e890.appspot.com",
-    messagingSenderId: "104420750350",
-    appId: "1:104420750350:web:e9827e5b8409ac13"
+  apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
